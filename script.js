24d5c8599300ca7ecf6445bbda4dead038112543
Chris W <wells457@umn.edu>
2020-05-23 23:41:04 -0500

    Half of the statuses are implemented

--- script.js
+++ script.js
@@ -66,6 +66,8 @@ var status1= [
 
 ]
 
+var currentStatus = status1;
+
 var status2= [
     "Nothing out of the oridinary",
     "Dead Werewolf, dog whistle",
@@ -75,7 +77,7 @@ var status2= [
     "Nothing to see here",
     "A Coffin",
     "A vaccum cleaner",
-    'Toilete Paper*',
+    'Toilet Paper*',
     "An open safe with a gun",
     "Nothing out of the oridinary",
     "Nothing out of the ordinary",
@@ -87,6 +89,12 @@ var status2= [
     "Dog with Squeky toy, stick on ground"
 
 ]
+
+
+
+
+
+
 
 var status3= [
     "Nothing out of the oridinary",
@@ -139,7 +147,7 @@ var placement=-2;
 
 $("#play").click(function(){
 
-    var input = $("#command").val();
+    var input = $("#command").val().toLowerCase();
     console.log(input)
 
     var message;
@@ -150,8 +158,160 @@ $("#play").click(function(){
             message = "The door is open"
             break;
             }
+        case "map_1":
+            if (input.includes("shoot") && inventory.includes("Loaded Gun") && currentStatus[1]===status1[1]){
+            message = "You shot the werewolf!"
+            currentStatus[1] = status2[1]
+            break;
+            }
+            else if (input.includes("whistle") && currentStatus[1]===status2[1]){
+                message = "You picked up the dog whistle!"
+                inventory.push("Dog_Whistle")
+                break;
+            }
+        case "map_3":
+            if (input.includes("silverware") &&  !inventory.includes("Silverware")){
+            message = "You picked up the silverware"
+            inventory.push("Silverware")
+            break;
+            }
+            else if (input.includes("fire") &&  inventory.includes("Unlit_Torch")){
+            message = "You lit your torch!"
+            inventory.push("Lit_Torch")  
+            inventory.replace("Unlit_Torch", "")
+            break;
+            }            
 
-    
+        case "map_5":
+            if ((input.includes("torch")  ||  input.includes("fire"))
+            && inventory.includes("Lit_Torch")
+            && currentStatus[4] === status1[4]){
+            message = "You set the mummy on fire!"
+            currentStatus[4] = status2[4]
+            break;
+            }
+            else if (input.includes("amulet") 
+            && !inventory.includes("Amulet")
+            && currentStatus[4] === status2[4]){
+            message = "You picked up the amulet!"
+            currentStatus[4] = status3[4]
+            break;
+            }            
+        case "map_6":
+            if (input.includes("clown")
+            && currentStatus[5] === status1[5]){
+            message = "You picked up the clown toy! (It squeaks!)"
+            inventory.push("Clown_Toy")
+            currentStatus[5] = status2[5]
+            break;
+            }
+        case "map_7":
+            if ((input.includes("kill") || input.includes("stake"))
+            && currentStatus[6] === status3[6]){
+            message = "You killed the vampire! You win!"
+            currentStatus[6] = status4[6]
+            break;
+            }
+            if ((input.includes("pillow") )
+            && currentStatus[6] === status1[6]){
+            message = "Under the pillow you see a small sheet of paper with the word SWORDFISH"
+            currentStatus[6] = status2[6]
+            inventory.push("Password")
+            break;
+            }
+            // if ((input.includes("vacuum") || input.includes("suck") )
+            // && currentStatus[7] === status1[7]
+            // && inventory.includes("Vacuum")){
+        case "map_8":
+            if ((input.includes("whistle") )
+            && currentStatus[7] === status1[7]
+            && inventory.includes("Dog_Whistle")){
+            message = "You blew the dog whistle, and the dog came and chewed up the skeleton"
+            currentStatus[7] = status2[7]
+            inventory.push("Password")
+            break;
+            }
+            if ((input.includes("vacuum") )
+            && currentStatus[7] === status2[7]){
+            message = "You picked up the vacuum cleaner"
+            currentStatus[7] = status3[7]
+            inventory.push("Vacuum")
+            break;
+            }
+        case "map_11":
+            if (currentStatus[8] === status1[8]
+            && input.includes("paper")){
+                currentStatus[8] = status3[8]
+                inventory.push("Toilet_Paper")
+                message = "You picked up toilet paper!"
+                break
+            }
+            if (currentStatus[8] === status1[8]
+                && input.includes("mirror")){
+                    currentStatus[8] = status2[8]
+                    inventory.push("Mirror")
+                    message = "You picked up a mirror!"
+                    break
+                }
+            if ((currentStatus[8] === status2[8] )
+            && input.includes("paper")){
+                message = "You picked up the toilet paper!"
+                currentStatus[8] = status4[8]
+                inventory.push("Toilet_Paper")
+                break;
+            }    
+            if ((currentStatus[8] === status3[8] )
+            && input.includes("mirror")){
+                message = "You picked up the Mirror!"
+                currentStatus[8] = status4[8]
+                inventory.push("Mirror")
+                break;
+            }
+        case "map_13":
+            if (input.includes("open")){
+            message = "The door is open"
+            break;
+            }
+        case "map_14":
+            if (input.includes("open")){
+            message = "The door is open"
+            break;
+            }
+        case "map_15":
+            if (input.includes("open")){
+            message = "The door is open"
+            break;
+            }
+        case "map_16":
+            if (input.includes("open")){
+            message = "The door is open"
+            break;
+            }
+        case "map_17":
+            if (input.includes("open")){
+            message = "The door is open"
+            break;
+            }
+        case "map_18":
+            if (input.includes("open")){
+            message = "The door is open"
+            break;
+            }
+        case "map_19":
+            if (input.includes("open")){
+            message = "The door is open"
+            break;
+            }
+        case "map_21":
+            if (input.includes("open")){
+            message = "The door is open"
+            break;
+            }
+        case "map_25":
+            if (input.includes("open")){
+            message = "The door is open"
+            break;
+            }
         default:
             message = "You can't do that"
             break;
